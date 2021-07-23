aa22ce8725cc679c8efb7c4a6c7403914f5ccffe
Cristiano Jr <cristianoandre96@gmail.com>
2021-07-23 20:23:19 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -10,8 +10,12 @@ export class ProdutoService {
     constructor(public http: HttpClient) {
     }
 
+    findById(produto_id : string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findByCategoria(categoria_id : string) {
-        return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
     }
 
     getSmallImageFromBucket(id : string) : Observable<any> {
@@ -19,4 +23,8 @@ export class ProdutoService {
         return this.http.get(url, {responseType : 'blob'});
     }
 
+    getImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
+        return this.http.get(url, {responseType : 'blob'});
+    }
 }
