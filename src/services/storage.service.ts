978a1aa63284c8356ffc03c420e82ac1b272ef08
Cristiano Jr <cristianoandre96@gmail.com>
2021-07-26 14:31:29 -0300

    Criando pagina de carrinho de compras

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEYS } from "../config/storage.keys.config";
+import { Cart } from "../models/cart";
 import { LocalUser } from "../models/local.user";
 
 @Injectable()
@@ -21,6 +22,25 @@ export class StorageService {
         }
         else {
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
+        }
+    }
+
+    getCart() : Cart {
+        let crt = localStorage.getItem(STORAGE_KEYS.cart);
+        if (crt == null){
+            return null;
+        }
+        else {
+            return JSON.parse(crt);
+        }
+    }
+
+    setCart( obj: Cart) {
+        if (obj == null) {
+            localStorage.removeItem(STORAGE_KEYS.cart);
+        }
+        else {
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
         }
     }
 }
